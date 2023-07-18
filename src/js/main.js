d64c66b039eb74ff090add41c03bdfda307bc455
Łukasz Beczek <lb@turis.pl>
2023-07-18 15:12:41 +0200

    Changes in UI, added some functions in JS to change colors

--- src/js/main.js
+++ src/js/main.js
@@ -1,19 +1,38 @@
 const container = document.querySelector('.squares-container')
 const gridSizeInput = document.querySelector('#grid-size')
-let isPressed = false
-gridSizeInput.value = 5
+const colorInput = document.querySelector('#color-input')
+const singleColorBtn = document.querySelector('#single-color') 
+const rainbowColorBtn = document.querySelector('#rainbow-color')
+const eraserBtn = document.querySelector('#eraser')
+const clearBtn = document.querySelector('#clear')
+
+let isMousePressed = false
+let isEraserOn = false
+gridSizeInput.value = 16
+
+const setColor = () => {
+	const singleColor = colorInput.value
+	return singleColor
+}
 
 const colorSquares = (e) => {
-	if (isPressed && container.contains(e.target)) {
-		e.target.style.backgroundColor = 'red'
+	if (isMousePressed && container.contains(e.target)) {
+		const singleColor = setColor()
+		e.target.style.backgroundColor = singleColor
 	}
 }
+
+// const eraseSquares = (e) => {
+// 	if (isMousePressed && container.contains(e.targer) && isEraserOn) {
+// 		e.target.style.backgroundColor = ''
+// 	}
+// }
 
 const setGridSize = () => {
 	container.innerHTML = ''
 	let gridSize = gridSizeInput.value
-    if (gridSize < 2 || gridSize > 10) {
-        gridSize = 5
+    if (gridSize < 2 || gridSize > 100) {
+        gridSize = 16
     } 
 	for (let i = 0; i < gridSize * gridSize; i++) {
 		const square = document.createElement('div')
@@ -24,10 +43,22 @@ const setGridSize = () => {
 	container.style.gridTemplateRows = `repeat(${gridSize}, 1fr)`
 }
 
+const clear = () => {
+	Array.from(container.children).forEach(child => {
+		child.style.backgroundColor = ''
+	})
+}
+
+const setSingleColor = () => {
+	
+}
+
 setGridSize()
-container.addEventListener('mousedown', () => (isPressed = true))
-container.addEventListener('mouseup', () => (isPressed = false))
-container.addEventListener('mouseleave', () => (isPressed = false))
+container.addEventListener('mousedown', () => (isMousePressed = true))
+container.addEventListener('mouseup', () => (isMousePressed = false))
+container.addEventListener('mouseleave', () => (isMousePressed = false))
 container.addEventListener('mousemove', colorSquares)
 container.addEventListener('mousedown', colorSquares)
 gridSizeInput.addEventListener('change', setGridSize)
+colorInput.addEventListener('change', setColor)
+clearBtn.addEventListener('click', clear)
